56ceff0e008ffceebe066be3747935d2a884784a
Krishna <nayak100krishna@gmail.com>
2023-03-21 13:53:41 +0530

    firebase ~

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBrWVZ7lvwVRRj-OHQcsCVJwNBT2vvgnGw",
-  authDomain: "social-4c592.firebaseapp.com",
-  projectId: "social-4c592",
-  storageBucket: "social-4c592.appspot.com",
-  messagingSenderId: "718336755683",
-  appId: "1:718336755683:web:0080e8d94cc9027ef442d6",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
