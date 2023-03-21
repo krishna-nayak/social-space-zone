bb815ea1afa8d3eba044c13c844db6a92fcd20ff
Krishna <nayak100krishna@gmail.com>
2023-03-21 13:49:10 +0530

    remove children obj

--- src/utils/PrivateWrapper.jsx
+++ src/utils/PrivateWrapper.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { isAuthenticated } from "./validation";
 
-export const PrivateWrapper = ({ children }) => {
+export const PrivateWrapper = () => {
   const auth = isAuthenticated();
   return auth ? <Outlet /> : <Navigate to="/login" />;
 };
